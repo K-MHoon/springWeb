28ef895127ddfa61b044a8fe30732b825e21aec9
K-MHoon <azmria117@gmail.com>
2021-05-22 15:51:04 +0900

    21.05.22 commit

--- src/main/java/com/ddinghoon/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/ddinghoon/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,17 @@
 package com.ddinghoon.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // DB Layer 접근자, Mybatis 등에서 Dao라고 불린다.
 // JPA에선 Repository라고 부르며, 인터페이스로 생성한다.
 // 기본적인 CRUD 메소드가 자동으로 생성된다.
 // JpaRepository<Entity 클래스, PK타입>
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    //SpringDataJpa에서 제공하지 않는 메소드는 아래처럼 쿼리로 작성해도 된다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/ddinghoon/springboot/service/posts/PostsService.java
+++ src/main/java/com/ddinghoon/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.ddinghoon.springboot.service.posts;
 
 import com.ddinghoon.springboot.domain.posts.Posts;
 import com.ddinghoon.springboot.domain.posts.PostsRepository;
+import com.ddinghoon.springboot.web.dto.PostsListResponseDto;
 import com.ddinghoon.springboot.web.dto.PostsResponseDto;
 import com.ddinghoon.springboot.web.dto.PostsSaveRequestDto;
 import com.ddinghoon.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신해준다.
 @RequiredArgsConstructor
@@ -27,8 +31,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        // deleteById 메소드를 이용하면 id로 삭제할 수도 있다.
+        // 존재하는 Posts인지 확인을 위해 엔티티 조회 후 그대로 삭제한다.
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    // readOnly = true를 주면 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선된다.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                // map(posts -> new PostsListResponseDto(posts))
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
